6eaa64040f049aac6a924dade00a5533759d425b
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-03-21 00:43:15 -0300

    Update setup_node.js

--- BdsManeger/files/setup_node.js
+++ BdsManeger/files/setup_node.js
@@ -2,33 +2,26 @@ const bds = require("/tmp/bds/index");
 const fs = require("fs");
 const path = require("path")
 bds.kill()
+
 if (process.env.TELEGRAM_TOKEN !== 'null') bds.telegram_token_save(process.env.TELEGRAM_TOKEN);
 else console.log('Telegram bot disabled, bot token not informed')
-const configs = JSON.stringify({
+
+bds.set_config(JSON.stringify({
     "description": process.env.DESCRIPTION,
     "name": process.env.WORLD_NAME,
     "gamemode": process.env.GAMEMODE,
     "difficulty": process.env.DIFFICULTY,
     "players": process.env.PLAYERS,
     "xbox": process.env.XBOX_ACCOUNT
-})
-bds.set_config(configs)
-setTimeout(() => {
-    var bds_software
-    if (fs.existsSync(path.join(bds.bds_dir_bedrock, "bedrock_server"))) bds_software = true
-    else if (fs.existsSync(path.join(bds.bds_dir_bedrock, "bedrock_server.exe"))) bds_software = true
-    else if (fs.existsSync(path.join(bds.bds_dir_java, "server.jar"))) bds_software = true
-    else bds_software = false
-    console.log("Server detetect:", bds_software)
-    function download_bds(){
-        if (process.env.BDS_VERSION == 'latest') var bds_download_version = "latest"
-        else var bds_download_version = process.env.BDS_VERSION
-        console.log(`downloading the version: ${bds_download_version}`)
-        bds.download(bds_download_version)
-    }
-    if (bds_software){
-        if (process.env.BDS_REINSTALL === "true") download_bds()
-        else console.log("Skipping reinstallation")
-    } else download_bds()
+}))
 
-}, 1700);
+var bds_software
+if (fs.existsSync(path.join(bds.bds_dir_bedrock, "bedrock_server"))) bds_software = true
+else if (fs.existsSync(path.join(bds.bds_dir_bedrock, "bedrock_server.exe"))) bds_software = true
+else if (fs.existsSync(path.join(bds.bds_dir_java, "server.jar"))) bds_software = true
+else bds_software = false
+console.log("Server detetect:", bds_software)
+if (bds_software){
+    if (process.env.BDS_REINSTALL === "true") bds.download(process.env.BDS_VERSION)
+    else console.log("Skipping reinstallation")
+} else bds.download(process.env.BDS_VERSION)
